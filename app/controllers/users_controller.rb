32a72495f06e8b297092da451c94c1c9952f8748
Starcatch <timurmishiev@gmail.com>
2020-05-16 19:35:51 -0400

    create a get signup method in UsersController

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+  
+  # signup route
+  # login route  
+  # logout route 
+
+  # the purpouse of signup route is to render the login page (form)
+
+  
+
+end 
