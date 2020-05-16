a21702bdc14c988025106ce157970b02aa2b91bb
Starcatch <timurmishiev@gmail.com>
2020-05-16 17:27:25 -0400

    add associations to my models

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,5 @@
 class Book < ActiveRecord::Base
-  
+  has_many :reviews
+  has_many :users, :through => :reviews
+  validates :name, :presence => true
 end
--- db/migrate/20200516204646_create_users_books_reviews.rb
+++ db/migrate/20200516204646_create_users_books_reviews.rb
@@ -1,4 +1,23 @@
 class CreateUsersBooksReviews < ActiveRecord::Migration
-  def change
+   def change
+     
+     create_table :users do |t|
+       t.string :name
+       t.string :password_digest
+    end 
+    
+    create_table :books do |t|
+      t.string :name
+      t.string :author
+      t.string :genre
+    end
+  
+      create_table :reviews do |t|
+        t.text :content
+        t.integer :user_id
+        t.integer :book_id
+      end
+
+    
   end
 end
