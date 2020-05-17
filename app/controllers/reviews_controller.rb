f58bd9e25a053786d7b8a6541ff807ccac856e5b
Starcatch <timurmishiev@gmail.com>
2020-05-17 01:49:51 -0400

    create reviews controllers methods, and views form related to them

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,22 @@
 class ReviewsController < ApplicationController
+    get 'reviews/:id/new' do
+      if logged_in?
+        @book = Book.find_by_id(params[:id])
+        erb :'reviews/new'
+      else
+        redirect '/login'
+      end
+    end 
+
+    post '/reviews/:book_id' do
+      @book = book_find_by_id(params[:book_id])
+      if params [:content].empty?
+        redirect "/review/#{@book.id}/new"
+      else 
+        Review.create(content: params[:content], book_id: params[:book_id], user_id: current_user.id)
+        redirect "/books/#{@book.id}"
+      end 
+    end 
+
+
 end 
