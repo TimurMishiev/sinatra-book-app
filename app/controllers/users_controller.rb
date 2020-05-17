96ba1397fd3904a8cd6c43553825b8ada10f476b
Starcatch <timurmishiev@gmail.com>
2020-05-16 20:28:03 -0400

    create methods for logging in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,13 +28,29 @@ class UsersController < ApplicationController
   end   
 
   
-  end 
+
 
 
   # the purpouse of  GET login route is to render the login page (form)
 
-
+  get '/login' do
+    if !logged_in?
+       erb :'users/login'
+    else
+       redirect'/cars'
+    end
+  end
+  
   #the purpouse of POST login route is to recieve  the login form.
   #find a user, and log the user in (create a session)
+  post '/login' do
+    user = User.find_by(:name params[:name])
+    if user && user.authenticate(params[:password])
+      sesions[:user_id] = user.id
+      redirect '/books'
+    end 
+     redirect '/login'
+  end 
+
 
 end 
