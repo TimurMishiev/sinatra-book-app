58f2e4504651dd809089426a80b410e1c9fd7131
Starcatch <timurmishiev@gmail.com>
2020-05-17 15:13:04 -0400

    add HTML to reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,30 @@
 class ReviewsController < ApplicationController
-    
+  
+  
   get '/reviews/:id/new' do
-      if logged_in?
+    if logged_in?
+        @book = Book.find_by_id(params[:id])
      erb:'reviews/new'
+    else 
+        redirect '/login'
     end 
   end 
+
+
+  post '/reviews/:book_id' do
+    @book = Book.find_by_id(params[:id])
+    if params[:content].empty?
+      redirect "/reviews/#{@book.id}/new"
+    else 
+      Review.create(content: params[:content], car_id: params[:book_id], user_id: current_user.id)
+      redirect "/books/#{@book.id}"
+    end 
+  end 
+
+  
+
+
+
 
 
 
