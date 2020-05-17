d8c6e1f92c457d5a7232d4a8b241d231e1b9867d
Starcatch <timurmishiev@gmail.com>
2020-05-17 14:49:02 -0400

    fix a route promlem between reviews controller and views file

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,22 +1,11 @@
 class ReviewsController < ApplicationController
-    get 'reviews/:id/new' do
+    
+  get '/reviews/:id/new' do
       if logged_in?
-        @book = Book.find_by_id(params[:id])
-        erb :'reviews/new'
-      else
-        redirect '/login'
-      end
+     erb:'reviews/new'
     end 
+  end 
 
-    post '/reviews/:book_id' do
-      @book = book_find_by_id(params[:book_id])
-      if params [:content].empty?
-        redirect "/review/#{@book.id}/new"
-      else 
-        Review.create(content: params[:content], book_id: params[:book_id], user_id: current_user.id)
-        redirect "/books/#{@book.id}"
-      end 
-    end 
 
 
 end 
