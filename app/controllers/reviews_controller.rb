11cf871cae759743016f86f2d5550a28def8d408
Starcatch <timurmishiev@gmail.com>
2020-05-17 21:01:59 -0400

    add method edit in controllers

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -16,16 +16,30 @@ class ReviewsController < ApplicationController
     if params[:content].empty?
       redirect "/reviews/#{@book.id}/new"
     else 
-      Review.create(content: params[:content], car_id: params[:book_id], user_id: current_user.id)
-      redirect "/books/#{@book.id}"
+      Review.create(content: params[:content], book_id: params[:book_id], user_id: current_user.id)
+      redirect '/books/1'
     end 
   end 
 
-  
+  get '/reviews/:id/edit' do
+    @review = Review.find_by_id(params[:id])
+    if @review && @review.user == current_user
+      erb :'reviews/edit'
+    else
+      redirect "reviews/#{@book.id}"
+    end 
+  end 
+
+  post 'reviews/:id/edit' do
+    @review = Review.find_by_id(params[:id])
+    if @review.user ==current_user
+      @review.update(content: params[:content])
+      redirect "/books"
+    end
+  end 
 
 
 
 
 
-
-end 
+  end 
