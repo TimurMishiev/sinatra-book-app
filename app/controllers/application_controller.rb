b11f6aaa9ddab3b60258ccc0696a07a43c040ff1
Starcatch <timurmishiev@gmail.com>
2020-05-18 21:56:40 -0400

    add rack-flash dependency

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,12 @@
 require './config/environment'
+require 'sinatra/base'
+require'rack-flash'
 
 class ApplicationController < Sinatra::Base
 
   
     configure do
+      use Rack::Flash
       set :public_folder, 'public'
       set :views, 'app/views'
       enable :sessions
