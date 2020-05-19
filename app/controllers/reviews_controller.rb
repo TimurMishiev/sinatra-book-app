d8a59aed0358bfcbac241058022cb114cbd9ea11
Starcatch <timurmishiev@gmail.com>
2020-05-19 01:42:32 -0400

    make website look pretty by refactoring flash messages

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,54 +1,51 @@
 class ReviewsController < ApplicationController
 
   get '/reviews/:id/new' do
-  if logged_in?
-    @book = Book.find_by_id(params[:id])
-    erb :'/reviews/new'
-  else
-    flash[:alert] = "You have to be logged in in order to add review"
-    redirect '/login'
-  end
+    if logged_in?
+        @book = Book.find_by_id(params[:id])
+       erb :'/reviews/new'
+    else
+         redirect '/login'
+    end
   end
   
   post '/reviews/:book_id' do
-  @book = Book.find_by_id(params[:book_id])
-  if params[:content].empty?
-  flash[:alert] = "Please add a review."
-  redirect "/reviews/#{@book.id}/new"
-  else
-  Review.create(content: params[:content], book_id: params[:book_id], user_id: current_user.id)
-    redirect "/books/#{@book.id}"
-  end
+       @book = Book.find_by_id(params[:book_id])
+    if params[:content].empty?
+        flash[:alert] = "Please add a review."
+        redirect "/reviews/#{@book.id}/new"
+    else
+        Review.create(content: params[:content], book_id: params[:book_id], user_id: current_user.id)
+        redirect "/books/#{@book.id}"
+    end
   end
   
   
   get '/reviews/:id/edit' do
-    @review = Review.find_by_id(params[:id])
+       @review = Review.find_by_id(params[:id])
     if @review &&  @review.user == current_user
-  erb :'reviews/edit'
-  else
-    flash[:alert] = 'You must be looged in as a different user to access that review'
-    redirect "reviews/#{@book.id}"
-    
-  end
+       erb :'reviews/edit'
+    else
+       flash[:alert] = 'You must be looged in as a different user to access that review'
+        redirect '/books'
+    end
   end
   
   post '/reviews/:id/edit' do
-  @review = Review.find_by_id(params[:id])
-  if @review.user == current_user
-    @review.update(content: params[:content])
-    flash[:alert] = "Review updated successfully!"
-    redirect "/books"
-  
-  end
+      @review = Review.find_by_id(params[:id])
+      @book = Book.find_by_id(params[:book_id])
+    if @review.user == current_user
+       @review.update(content: params[:content])
+        redirect '/books'
+    end
   end
   
   get '/reviews/:id/delete'  do
-    @review = Review.find_by_id(params[:id])
+       @review = Review.find_by_id(params[:id])
     if logged_in? && @review.user == current_user
-    @review.delete
-    redirect '/books'
-  end
+       @review.delete
+       redirect '/books'
+    end
   end
   
   
